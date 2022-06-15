27df557ca40d185ecf38df5f6042cf1d84383095
Adrien Graux <adrien.graux@octo.com>
2022-06-15 22:28:07 +0200

    api key solution

--- main.js
+++ main.js
@@ -2,7 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const API_KEY = '395a9cce-6ffc-48b8-8372-b95089c3abc6'
+
 app.get('/', (req, res) => {
+  const apiKeyFromHeader = req.headers['x-api-key']
+  if (API_KEY !== apiKeyFromHeader) {
+    res.sendStatus(401)
+    return
+  }
   const result = {
     success: true
   }
